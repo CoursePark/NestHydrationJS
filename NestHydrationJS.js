283ff2b6fd9899158b08859cb16805837b09849d
David Valentiate <me@davidv.ca>
2014-08-08 13:06:40 -0400

    completed propertyMappingFromColumnHints method and speced it

--- NestHydrationJS.js
+++ NestHydrationJS.js
@@ -1,106 +1,101 @@
-(function() {
-	var NestHydrationJS;
+'use strict';
+
+var NestHydrationJS, _;
+
+_ = require('lodash');
+
+NestHydrationJS = {};
+
+NestHydrationJS.nest = function (table, propertyMapping) {
+	var listOnEmpty = false;
 	
-	_ = require('lodash');
+	if (propertyMapping == null) {
+		propertyMapping = null;
+	}
 	
-	NestHydrationJS = (function() {
+	if (table === null) {
+		return null;
+	}
+	
+	if (!_.isArray(table) && !_.isPlainObject(table)) {
+		throw 'nest expects param table to be an array or plain object';
+	}
+	
+	if (!_.isArray(propertyMapping) && propertyMapping !== null && propertyMapping !== true) {
+		throw 'nest expects param propertyMapping to be an array, null, or true';
+	}
+	
+	// propertyMapping can be set to true as a tie break between
+	// returning null (empty structure) or an empty list
+	if (propertyMapping === true) {
+		listOnEmpty = true;
+		propertyMapping = null;
+	} else if (_.isArray(propertyMapping)) {
+		listOnEmpty = true;
+	}
+	
+	if (table.length === 0) {
+		return listOnEmpty ? array() : null;
+	}
+	
+	if (_.isPlainObject(table)) {
+		// internal table should be a table format but a plaing object
+		// could be passed as the first (and only) row of that table
+		table = [table];
+	}
+	
+	if (propertyMapping === null) {
+		// property mapping not specified, determine it from column names
+		propertyMapping = this.propertyMappingFromColumnHints(_.keys(table[0]));
+	}
+	
+	if (propertyMapping === null) {
+		// properties is empty, can't form structure or determine content
+		// for a list. Assume a structure unless listOnEmpty
+		return listOnEmpty ? [] : null;
+	}
+};
+
+/* Returns a property mapping data structure based on the names of columns
+ * in columnList. Used internally by nest when its propertyMapping param
+ * is not specified.
+ */
+NestHydrationJS.propertyMappingFromColumnHints = function (columnList) {
+	var propertyMapping, prop, i, column, pointer, navList, j, nav;
+	
+	propertyMapping = {base: null};
+	
+	for (i = 0; i < columnList.length; i++) {
+		column = columnList[i];
 		
-		NestHydrationJS.prototype.nest = function(table, propertyMapping) {
-			var listOnEmpty = false;
-			
-			if (propertyMapping == null) {
-				propertyMapping = null;
-			}
-			
-			if (table === null) {
-				return null;
-			}
-			
-			if (!_.isArray(table) && !_.isPlainObject(table)) {
-				throw 'nest expects param table to be an array or plain object';
-			}
-			
-			if (!_.isArray(propertyMapping) && propertyMapping !== null && propertyMapping !== true) {
-				throw 'nest expects param propertyMapping to be an array, null, or true';
-			}
-			
-			// propertyMapping can be set to true as a tie break between
-			// returning null (empty structure) or an empty list
-			if (propertyMapping === true) {
-				listOnEmpty = true;
-				propertyMapping = null;
-			} else if (_.isArray(propertyMapping)) {
-				listOnEmpty = true;
-			}
-			
-			if (table.length === 0) {
-				return listOnEmpty ? array() : null;
-			}
-			
-			if (_.isPlainObject(table)) {
-				// internal table should be a table format but a plaing object
-				// could be passed as the first (and only) row of that table
-				table = [table];
-			}
-			
-			if (propertyMapping === null) {
-				// property mapping not specified, determine it from column names
-				propertyMapping = this.propertyMappingFromColumnHints(_.keys(table[0]));
-			}
-			
-			if (_.isEmpty(propertyMapping)) {
-				// properties is empty, can't form structure or determine content
-				// for a list. Assume a structure unless listOnEmpty
-				return listOnEmpty ? [] : null;
-			}
-		};
+		pointer = propertyMapping; // point to base on each new column
+		prop = 'base';
 		
-		/* Returns a property mapping data structure based on the names of columns
-		 * in columnList. Used internally by nest when its propertyMapping param
-		 * is not specified.
-		 */
-		NestHydrationJS.prototype.propertyMappingFromColumnHints = function(columnList) {
-			var propertyMapping, i, column, pointer, leaf, j, nav;
+		navList = column.split('_');
+		
+		for (j = 0; j < navList.length; j++) {
+			nav = navList[j];
 			
-			if (columnList.length === 0 && columnList[0].substr(0, 1) === '_') {
-				// column name starts with a '_', base element is an array
-				propertyMapping = [];
-			} else {
-				// base element is a property list
-				propertyMapping = {};
-			}
-			
-			for (i = 0; i < columnList.length; i++) {
-				column = columnList[i];
-				
-				pointer = propertyMapping; // point to base on each new column
-				
-				navList = column.split('_');
-				leaf = navList.pop();
-				
-				for (j = 0; j < navList.length; j++) {
-					nav = navList[j];
-					
-					if (nav === '') {
-						nav = 0;
-					}
-########### trying to sort things out right here
-########### main issue is that the following code made use of PHPs arrays being the same as objects, JS does not work that way					
-					if (!array_key_exists($nav, $pointer)) {
-						$pointer[$nav] = array();
-					}
-					
-					pointer = pointer[nav];
+			if (nav === '') {
+				if (pointer[prop] === null) {
+					pointer[prop] = [null];
 				}
-				pointer[leaf] = column;
+				pointer = pointer[prop];
+				prop = 0;
+			} else {
+				if (pointer[prop] === null) {
+					pointer[prop] = {};
+				}
+				if (typeof pointer[prop][nav] === 'undefined') {
+					pointer[prop][nav] = null;
+				}
+				pointer = pointer[prop];
+				prop = nav;
 			}
-			
-			return propertyMapping;
-		};
-		
-		return NestHydrationJS;
-	})();
+		}
+	}
 	
-	module.exports = NestHydrationJS;
-	
-}).call(this);
+	return propertyMapping.base;
+};
+
+module.exports = NestHydrationJS;
--- spec/NestHydrationJS.spec.js
+++ spec/NestHydrationJS.spec.js
@@ -1,2 +1,141 @@
-NestHydrationJS = require('../lib/Resource');
+'use strict';
 
+var NestHydrationJS = require('../NestHydrationJS');
+
+describe('NestHydrationJS', function () {
+	describe('propertyMappingFromColumnHints method', function () {
+		describe('passed empty as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				expect(result).toBeNull();
+			});
+		});
+		
+		describe('passed single direct property as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [
+					'a'
+				];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				var expected = {
+					a: null
+				};
+				expect(result).toEqual(expected);
+			});
+		});
+		
+		describe('passed multiple direct properties as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [
+					'a',
+					'b'
+				];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				var expected = {
+					a: null,
+					b: null
+				};
+				expect(result).toEqual(expected);
+			});
+		});
+		
+		describe('passed single many relation property as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [
+					'_a'
+				];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				var expected = [{
+					a: null
+				}];
+				expect(result).toEqual(expected);
+			});
+		});
+		
+		describe('passed multiple many relation properties as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [
+					'_a',
+					'_b'
+				];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				var expected = [{
+					a: null,
+					b: null
+				}];
+				expect(result).toEqual(expected);
+			});
+		});
+		
+		describe('passed 2nd level depth on simple properties as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [
+					'a',
+					'b_c'
+				];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				var expected = {
+					a: null,
+					b: {
+						c: null
+					}
+				};
+				expect(result).toEqual(expected);
+			});
+		});
+		
+		describe('passed complex scenaro as columnList', function () {
+			var result;
+			beforeEach(function () {
+				var columnList = [
+					'_id',
+					'_a_id',
+					'_a_b',
+					'_a_c__id',
+					'_a_c__d'
+				];
+				result = NestHydrationJS.propertyMappingFromColumnHints(columnList);
+			});
+			
+			it('should return null', function () {
+				var expected = [{
+					id: null,
+					a: {
+						id: null,
+						b: null,
+						c: [{
+							id: null,
+							d: null
+						}]
+					}
+				}];
+				expect(result).toEqual(expected);
+			});
+		});
+	});
+});
